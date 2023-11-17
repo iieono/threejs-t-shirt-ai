4c35576a01645fca6c430bb21f2f009351a430a7
iieono <itstianmi@gmail.com>
2023-11-17 20:48:27 +0500

    first commit

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -58,7 +58,7 @@ function Customizer() {
         }
         )
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://project-ai-t-shirt-ai.onrender.com/api/v1/dalle', {
         method: "POST",
         headers : {
           'Content-Type': 'application/json'
